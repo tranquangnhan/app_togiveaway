795f3b9453bb34eae476ebbd016b271d759a5939
dmngtj <btcgiaivnsvntopencup@gmail.com>
2021-05-13 10:35:07 +0700

    addaccount

--- api/index.php
+++ api/index.php
@@ -15,7 +15,37 @@ if (isset($_GET['act']))
             $Return['data']= $blogs->showAllBlog();
             echo json_encode($Return);
             break;
-        
+        case 'addblog':
+            $Return = array();
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $Return['data']= array("name"=>$name,"email"=>$email);
+                echo json_encode($Return);
+            }
+            break;
+        case 'addacount':
+            $Return = array();
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+               
+                $id = $_POST['id'];
+                $email = $_POST['email'];
+                $kind = $_POST['kind'];
+                if($blogs->addAccount($id,$email,0,$kind)){
+                    // http_response_code(200);
+                    // echo json_encode(array("message" => "User was successfully registered."));
+                    $Return['statusCode'] =1;
+                    $Return['message'] ='success';
+                }else{
+                    $Return['statusCode'] =0;
+                    $Return['message'] ='error';
+                    // http_response_code(400);
+
+                    // echo json_encode(array("message" => "Unable to register the user."));
+                }
+                echo json_encode($Return);
+            }
+            break;
         default:
             # code...
             break;
